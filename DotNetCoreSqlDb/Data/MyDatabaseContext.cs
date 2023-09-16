fb5a3c85d1c10a9a80e5a228da851c34874df679
Tom Keller <144012938+tomkPFRS@users.noreply.github.com>
2023-09-15 22:18:00 -0500

    configure user impersonation

--- DotNetCoreSqlDb/Data/MyDatabaseContext.cs
+++ DotNetCoreSqlDb/Data/MyDatabaseContext.cs
@@ -9,9 +9,15 @@ namespace DotNetCoreSqlDb.Data
 {
     public class MyDatabaseContext : DbContext
     {
-        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
-            : base(options)
+        // public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+        //     : base(options)
+        // {
+        // }
+        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options, IHttpContextAccessor accessor)
+           : base(options)
         {
+            var conn = Database.GetDbConnection() as SqlConnection;
+            conn.AccessToken = accessor.HttpContext.Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"];
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; } = default!;
