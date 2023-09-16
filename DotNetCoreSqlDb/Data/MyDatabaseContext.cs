9752837d999dfe3c917e4a059dd3ceddf5a6c598
Tom Keller <144012938+tomkPFRS@users.noreply.github.com>
2023-09-15 22:47:59 -0500

    using System.Data.SqlClient for SqlConnection

--- DotNetCoreSqlDb/Data/MyDatabaseContext.cs
+++ DotNetCoreSqlDb/Data/MyDatabaseContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Models;
+using System.Data.SqlClient;
 
 namespace DotNetCoreSqlDb.Data
 {
